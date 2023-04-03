d5abea7e92d8813afe12beb0521257b68fedef49
Christoph Obst <christoph.obst@posteo.de>
2023-04-03 11:42:15 +0200

    Capstone-slot-machine

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,58 @@
-import GlobalStyle from "../styles";
+import styled from "styled-components";
+
+const HeadlineContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+`;
+
+const WheelContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100px;
+  align-items: center;
+`;
+
+const StyledBox1 = styled.div`
+  background: lightgrey;
+  width: 100px;
+  height: 200px;
+`;
+const StyledBox2 = styled.div`
+  background: lightgrey;
+  width: 100px;
+  height: 200px;
+`;
+const StyledBox3 = styled.div`
+  background: lightgrey;
+  width: 100px;
+  height: 200px;
+`;
+const StyledButton = styled.button`
+  width: 50px;
+  height: 25px;
+`;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <HeadlineContainer>
+        <h1>Slot Machine</h1>
+      </HeadlineContainer>
+      <WheelContainer>
+        <StyledBox1 />
+        <StyledBox2 />
+        <StyledBox3 />
+      </WheelContainer>
+      <ButtonContainer>
+        <StyledButton>Start</StyledButton>
+      </ButtonContainer>
       <Component {...pageProps} />
     </>
   );
